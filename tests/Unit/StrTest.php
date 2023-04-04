e28544d1b8b7cc9f69d8b2f49cfaf47ee9ac3b69
Ashley Hardy <a.hardy@taurusnetwork.co.uk>
2023-04-04 10:19:51 +0100

    tests: add remaining tests for str tests

--- tests/Unit/StrTest.php
+++ tests/Unit/StrTest.php
@@ -2,14 +2,47 @@
 
 use AshleyHardy\Utilities\Str;
 
-test('Str::camelToKebab', function() {
-    expect(Str::camelToKebab('helloWorld'))->toEqual('hello-world');
+test('Str::kebabToCamel', function() {
+    expect(Str::kebabToCamel('hello-world'))->toEqual('helloWorld');
+});
+
+test('Str::snakeToCamel', function() {
+    expect(Str::snakeToCamel('hello_world'))->toEqual('helloWorld');
 });
 
 test('Str::camelToSnake', function() {
     expect(Str::camelToSnake('helloWorld'))->toEqual('hello_world');
 });
 
+test('Str::camelToKebab', function() {
+    expect(Str::camelToKebab('helloWorld'))->toEqual('hello-world');
+});
+
+test('Str::kebabToPascal', function() {
+    expect(Str::kebabToPascal('hello-world'))->toEqual('HelloWorld');
+});
+
+test('Str::snakeToPascal', function() {
+    expect(Str::snakeToPascal('hello_world'))->toEqual('HelloWorld');
+});
+
 test('Str::camelToPascal', function() {
     expect(Str::camelToPascal('helloWorld'))->toEqual('HelloWorld');
+});
+
+test('Str::tokenise can process a list', function() {
+    $string = "{0} {1}";
+    $tokens = ['Hello', 'World'];
+
+    expect(Str::tokenise($string, $tokens))->toEqual('Hello World');
+});
+
+test('Str::tokenise can process an associative array', function() {
+    $string = "{{ greeting }} {{ noun }}";
+    $tokens = [
+        'greeting' => 'Whaddup',
+        'noun' => 'yo'
+    ];
+
+    expect(Str::tokenise($string, $tokens))->toEqual('Whaddup yo');
 });
